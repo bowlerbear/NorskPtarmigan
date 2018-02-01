c7584ac35b4fda064e3904d52323760745266199
Diana Bowler <diana.bowler@NINSRV16.NINA.NO>
2018-02-01 10:34:37 +0100

    Diana restructured the folders

---
+++ script/effectsizeComparison.R
@@ -0,0 +1,16 @@
+#read in each output file, extract the coefficients and divide by the standard error of the associated variable
+
+load("out1_springOnset.RData")
+out1$summary[grepl("beta",row.names(out1$summary)),]
+
+load("out1_springTemp.RData")
+out1$summary[grepl("beta",row.names(out1$summary)),]
+
+load("out1_juneTemp.RData")
+out1$summary[grepl("beta",row.names(out1$summary)),]
+
+load("out1_winterTemp.RData")
+out1$summary[grepl("beta",row.names(out1$summary)),]
+
+load("out1_rodent.RData")
+out1$summary[grepl("beta",row.names(out1$summary)),]
